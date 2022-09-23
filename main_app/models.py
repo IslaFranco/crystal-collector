e95bc2ca8504efadf694a07f3b42ebf893cf54ba
islafranco <isla_franco@icloud.com>
2022-09-22 20:32:30 -0700

    detail page works added model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Crystal(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=800)
+    properties = models.CharField(max_length=100)
+    chakras = models.CharField(max_length=100)
+    zodiac = models.CharField(max_length=100)
+    color = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+#Anytime you mkae changes to our models, we need to make migrations to represent your data to your database
+        
--- main_app/views.py
+++ main_app/views.py
@@ -1,26 +1,17 @@
 from django.shortcuts import render
+from .models import Crystal
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html', {'page_name': 'Home'})
 
 def about(request):
-    return render(request, 'about.html')
-
-class Crystal:
-    def __init__(self, name, description, properties, chakras, zodiac, color):
-        self.name = name
-        self.description = description
-        self.properties = properties
-        self.chakras = chakras
-        self.zodiac = zodiac
-        self.color = color
-
-crystals = [
-    Crystal('Amazonite', 'Aligning your values', 'Balance, Calm, and Clarity', 'Heart and Throat', 'Virgo', 'Pearly Brihgt turquoise-green with white fine veins'),
-    Crystal('Black Obsidian', 'Empowers inner strength to see destructive behavioural patterns', 'Grounding, Intuition, Protection', 'Root', 'Scorpio and Saggitarius', 'Black Glass Like'),
-    Crystal('Blue Lace Agate', 'Increases confidence to clearly communicate', 'Balance, Calm, Communication', 'Throat', 'Gemini and Pisces', 'Light Blue with white and lighter tones'),
-]        
+    return render(request, 'about.html')       
 
 def crystals_index(request):
+    crystals = Crystal.objects.all()
     return render(request, 'crystals/index.html', { 'crystals': crystals })
+
+def crystals_detail(request, crystal_id):
+    crystal = Crystal.objects.get(id=crystal_id)
+    return render(request, 'crystals/detail.html', {'crystal': crystal})
